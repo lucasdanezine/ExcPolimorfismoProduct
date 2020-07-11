9ad538ddd9c112379b75085fe2e4b0a90d85ebfd
Lucas <ldanezine@gmail.com>
2020-07-11 02:25:49 -0300

    Implementada a classe UsedProduct com atributos e métodos.

---
+++ ExcPolimorfismoProduct/Entities/UsedProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcPolimorfismoProduct.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime Manufacture { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufacture) : base (name, price)
+        {
+            Manufacture = manufacture;
+        }
+
+        public sealed override string PriceTag()
+        {
+            return base.PriceTag() + "Manufacture date: "+ Manufacture;
+        }
+    }
+}
