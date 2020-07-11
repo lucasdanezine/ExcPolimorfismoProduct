92762dbc8d4fd146dffbcf8dc364e99dc577396e
Lucas <ldanezine@gmail.com>
2020-07-11 02:18:59 -0300

    Implementado a classe para produtos importados com atributos e métodos.

---
+++ ExcPolimorfismoProduct/Entities/ImportedProduct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ExcPolimorfismoProduct.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFee) : base  (name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public sealed override string PriceTag()
+        {
+            return base.PriceTag() + "(Customs fee:  $ "+CustomsFee.ToString("F2",CultureInfo.InvariantCulture) ;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+
+    }
+}
